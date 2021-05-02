0a31c243ca2944cf2475e02f4214056878dd6b8b
James Perrin <jamesperrin88@gmail.com>
2021-05-02 11:09:03 +1000

    Trying to fix hex stufff

--- app/controllers/converters_controller.rb
+++ app/controllers/converters_controller.rb
@@ -1,20 +1,34 @@
 class ConvertersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def index
-  end
+  def index; end
 
   def show
-  @answer = decimal_to_binary(params[:Number].to_i, params[:Base].to_i)
+    @answer = decimal_to_binary(params[:Number].to_i, params[:Base].to_i)
   end
-end
 
-def decimal_to_binary(decimal, base = 2)
-  arr = []
-  until decimal.zero?
-    number = decimal % base
-    arr << number
-    decimal /= base
+  def decimal_to_binary(decimal, base = 2)
+    arr = []
+    until decimal.zero?
+      number = decimal % base
+      arr << number
+      decimal /= base
+    end
+    arr.length.zero? ? "0" : hash(arr).reverse.join
   end
-  arr.length.zero? ? "0" : arr.reverse.join
+
+  def hash(arr)
+    hash = { A: 10, B: 11, C: 12, D: 13, E: 14, F: 15 }
+
+    arr.map do |number|
+      hash.map do |key, value|
+        if number == value
+          number = key.to_s
+          p number
+        end
+      end
+    end
+    p arr
+    arr
+  end
 end
